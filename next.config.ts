d4dd4444442e7e3d15d9bfa2ceab9c4b27139a72
Noufal <noufalkakdlr@gmail.com>
2026-07-13 20:14:34 +0530

    add export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        port: "",
         pathname: "/**",
       },
     ],
